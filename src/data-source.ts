1f15d3f260c7a5e8d3ff8df75d5d9ec64541fde0
Aman Khemka <amankhemka.ak28@gmail.com>
2024-10-26 12:46:57 +0530

    fix(db): update db env setup for docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,13 @@ import { Contact } from './entity/Contact';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
+  host: process.env.POSTGRES_HOST || 'db',
+  url: process.env.DATABASE_URL,
   port: 5432,
   username: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DB || 'postgres',
   entities: [Contact],
   synchronize: true,
+  poolSize: 250,
 });
